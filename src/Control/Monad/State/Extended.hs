a9e0d2bd4b94f8fd70ac2a2cf3ee9cc26f45d890
Michalis Pardalos <m.pardalos@gmail.com>
2018-10-12 20:13:14 +0100

    Define withState for MonadState

---
+++ src/Control/Monad/State/Extended.hs
@@ -0,0 +1,13 @@
+module Control.Monad.State.Extended (
+    module Control.Monad.State,
+    withState
+) where
+
+import Control.Monad.State hiding (withState)
+
+withState :: MonadState s m => (s -> s) -> m a -> m a
+withState f a = do
+    originalState <- get
+    modify f *> a <* modify (const originalState)
+
+
