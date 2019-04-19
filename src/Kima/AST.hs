e498b0ba8eeafbfef8baf959ad7f26b2c278fe0a
Michalis Pardalos <m.pardalos@gmail.com>
2019-04-20 00:07:04 +0300

    Separate Typenames

--- src/Kima/AST.hs
+++ src/Kima/AST.hs
@@ -2,4 +2,5 @@ module Kima.AST (module E) where
 
 import Kima.AST.AST as E
 import Kima.AST.Names as E
+import Kima.AST.Types as E
 import Kima.AST.Kinds as E
---
+++ src/Kima/AST/Types.hs
@@ -0,0 +1,23 @@
+-- | Types in the AST
+
+module Kima.AST.Types where
+
+import Data.Text.Prettyprint.Doc
+
+type TypeName = String
+
+data TypeExpr
+    -- | Just a single type
+    = TypeName TypeName
+    -- | Function signature
+    | SignatureType [TypeExpr] TypeExpr
+    deriving Eq
+
+instance Show TypeExpr where
+    show (TypeName s) = "#\"" ++ s ++ "\""
+    show (SignatureType args rt) = "#( (" ++ show args ++ ") -> " ++ show rt ++ ")"
+
+instance Pretty TypeExpr where
+    pretty (TypeName name) = pretty name
+    pretty (SignatureType args returnType) =
+        tupled (pretty <$> args) <+> "->" <+> pretty returnType
