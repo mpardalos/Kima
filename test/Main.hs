29c152d1e278fc204c16b450a70f70bd5fa842ee
Michalis Pardalos <m.pardalos@gmail.com>
2019-04-21 14:41:56 +0300

    Change test order
    Parser first, file last. If the parser is wrong then it might be the
    reason other tests fail as well

--- test/Main.hs
+++ test/Main.hs
@@ -11,14 +11,11 @@ import           Kima.Test.Parser              as Parser
 
 main :: IO ()
 main = do
-        args <- getArgs
-        let config = defaultConfig
-                    { configFormatter = if "--junit-output" `elem` args
-                                                then Just xmlFormatter
-                                                else Nothing
-                    }
-        withArgs (filter (/= "--junit-output") args) $ hspecWith config $ do
-                FileTests.spec
-                ConstraintSolver.spec
-                DomainCalculation.spec
-                Parser.spec
+    args <- getArgs
+    let config =
+            defaultConfig { configFormatter = if "--junit-output" `elem` args then Just xmlFormatter else Nothing }
+    withArgs (filter (/= "--junit-output") args) $ hspecWith config $ do
+        Parser.spec
+        DomainCalculation.spec
+        ConstraintSolver.spec
+        FileTests.spec
