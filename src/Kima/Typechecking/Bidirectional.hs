024459b351d693875c252dfe1743e5795ab36900
Michalis Pardalos <m.pardalos@gmail.com>
2019-08-10 16:05:48 +0100

    Implement statement checking (not assignment)

--- src/Kima/Typechecking/Bidirectional.hs
+++ src/Kima/Typechecking/Bidirectional.hs
@@ -72,6 +72,67 @@ check expectedType expr = do
     inBinding :: KType -> Binding -> Bool
     inBinding t (Binding _ ts) = Set.member t ts
 
+--------------------------------
+---------- Statements ----------
+--------------------------------
+
+inferReturns :: MonadTC m => AST 'Stmt TypeAnnotated -> m (Set KType)
+inferReturns (ExprStmt expr                         ) = types <$> infer expr
+inferReturns (Block    stmts                        ) = withState id $ do
+    statementTypes <- mapM inferReturns stmts
+    return $ lastDef (Set.singleton KUnit) statementTypes
+inferReturns (While    (WhileStmt cond blk         )) = do
+    check KBool cond
+    _doesntMatter <- inferReturns blk
+    return (Set.singleton KUnit)
+inferReturns (If       (IfStmt cond thenBlk elseBlk)) = do
+    check KBool cond
+    thenBlkType <- inferReturns thenBlk
+    elseBlkType <- inferReturns elseBlk
+    assert
+        (not $ null (thenBlkType `Set.intersection` elseBlkType))
+        (  "True branch returns "
+        <> show thenBlkType
+        <> " false branch returns "
+        <> show elseBlkType
+        )
+    return (thenBlkType `Set.intersection` elseBlkType)
+
+inferReturns (Assign name expr                      ) = do
+    Binding _              inferedTypes <- infer expr
+    Binding nameMutability nameTypes    <- _lookupName name
+
+    assert (nameMutability == Variable) (show name <> " is constant ")
+    assert (not $ null (nameTypes `Set.intersection` inferedTypes))
+           "Can't assign a value of "
+    return (Set.singleton KUnit)
+inferReturns (Var _name _declaredType _expr         ) = _inferVar
+inferReturns (Let _name _declaredType _expr         ) = _inferLet
+
+checkReturns :: MonadTC m => KType -> AST 'Stmt TypeAnnotated -> m ()
+checkReturns t (ExprStmt expr) = check t expr
+checkReturns t blk@Block{}     = do
+    returnTypes <- inferReturns blk
+    assert (Set.member t returnTypes)
+           ("The return type is not one of" <> show returnTypes)
+checkReturns t stmt@While{} = do
+    _ <- inferReturns stmt
+    assert (t == KUnit) "While block always returns Unit"
+checkReturns t stmt@If{} = do
+    returnTypes <- inferReturns stmt
+    assert
+        (Set.member t returnTypes)
+        ("If block returns one of" <> show returnTypes <> ", not " <> show t)
+checkReturns t stmt@Assign{} = do
+    _ <- inferReturns stmt
+    assert (t == KUnit) "Assignment always returns Unit"
+checkReturns t stmt@Var{} = do
+    _ <- inferReturns stmt
+    assert (t == KUnit) "Var always returns Unit"
+checkReturns t stmt@Let{} = do
+    _ <- inferReturns stmt
+    assert (t == KUnit) "Let always returns Unit"
+
 -----------------------------
 ---------- Helpers ----------
 -----------------------------
