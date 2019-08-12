6360b85e5f253a4ba5d2b9eeeba733cf41f95752
Michalis Pardalos <m.pardalos@gmail.com>
2019-08-12 22:39:18 +0100

    Refactor and add docs to Bidirectional.hs

--- src/Kima/Typechecking/Bidirectional.hs
+++ src/Kima/Typechecking/Bidirectional.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE OverloadedLists #-}
 module Kima.Typechecking.Bidirectional
     ( MonadTC
-    , runTypeChecking
-    , checkProgram
-    , checkTopLevel
     , infer
     , check
     , inferReturns
     , checkReturns
+    , checkProgram
+    , checkTopLevel
+    , runTypeChecking
     )
 where
 
@@ -23,11 +23,11 @@ where
 
 import           Safe
 
-import           Control.Monad.State.Extended
 import           Control.Monad.Except
+import           Control.Monad.State.Extended
 
-import           Data.Functor
 import           Data.Bifunctor
+import           Data.Functor
 import           Data.Maybe
 import qualified Data.Map                      as Map
 import           Data.Set                       ( Set )
@@ -35,8 +35,8 @@ import qualified Data.Set                      as Set
 
 import           Kima.AST
 import           Kima.KimaTypes
-import           Kima.Typechecking.TypeCtx
 import           Kima.Typechecking.Errors
+import           Kima.Typechecking.TypeCtx
 
 type MonadTC m = (MonadState TypeCtx m, MonadError TypecheckingError m)
 
@@ -46,14 +46,19 @@ runTypeChecking ctx action = evalStateT action ctx
 ---------- Expressions ----------
 ---------------------------------
 
+-- | Try to infer the type of an expression. Returns the typed expression as
+-- well as its type. Throws an error if a type can't be assigned to the
+-- expression *or* if there are multiple possible types
 infer :: MonadTC m => AST 'Expr TypeAnnotated -> m (AST 'Expr Typed, KType)
-infer (     LiteralE    lit@(IntExpr    _)) = pure (LiteralE lit, KInt)
-infer (     LiteralE    lit@(FloatExpr  _)) = pure (LiteralE lit, KFloat)
-infer (     LiteralE    lit@(BoolExpr   _)) = pure (LiteralE lit, KBool)
-infer (     LiteralE    lit@(StringExpr _)) = pure (LiteralE lit, KString)
-infer expr@(IdentifierE name) = enumerateTypes expr <&> Set.toList >>= \case
-    -- | There needs to be only a single possibility for the type. Otherwise we
-    -- | have an ambiguity
+infer (LiteralE    lit@(IntExpr    _)) = pure (LiteralE lit, KInt)
+infer (LiteralE    lit@(FloatExpr  _)) = pure (LiteralE lit, KFloat)
+infer (LiteralE    lit@(BoolExpr   _)) = pure (LiteralE lit, KBool)
+infer (LiteralE    lit@(StringExpr _)) = pure (LiteralE lit, KString)
+infer (IdentifierE name) = (lookupName name <&> types) <&> Set.toList >>= \case
+    -- There needs to be only a single possibility for the type. Otherwise we
+    -- have an ambiguity
+    -- Also. No need to check for empty case since that would have thrown an
+    -- error in lookupName
     [t]   -> pure (IdentifierE (typeAnnotate t name), t)
     types -> throwError (AmbiguousName name types)
 infer (FuncExpr args rt body) = do
@@ -63,23 +68,23 @@ infer (FuncExpr args rt body) = do
     let typedFuncExpr = FuncExpr args rt typedBody
     return (typedFuncExpr, functionType)
 infer (Call callee args) = do
-    calleeTypes <- Set.toList <$> enumerateTypes callee
+    calleeTypes     <- Set.toList <$> enumerateTypes callee
 
-    possibleResults :: [Maybe (AST 'Expr Typed, KType)] <-
-        forM calleeTypes $ \case
-            calleeType@(KFunc (Signature argTypes returnType)) ->
-                do
-                        typedArgs   <- zipWithM check argTypes args
-                        typedCallee <- check calleeType callee
-                        return $ Just (Call typedCallee typedArgs, returnType)
-                    `catchError` const (pure Nothing)
-            _ -> pure Nothing
+    possibleResults <- forM calleeTypes $ \case
+        calleeType@(KFunc (Signature argTypes returnType)) ->
+            do
+                    typedArgs   <- zipWithM check argTypes args
+                    typedCallee <- check calleeType callee
+                    return $ Just (Call typedCallee typedArgs, returnType)
+                `catchError` const (pure Nothing)
+        _ -> pure Nothing
 
     case catMaybes possibleResults of
         [        result] -> return result
         results@(_ : _ ) -> throwError (AmbiguousCall (snd <$> results))
         []               -> throwError NoMatchingFunction
 
+-- | List all possible types for an expression
 enumerateTypes :: MonadTC m => AST 'Expr TypeAnnotated -> m (Set KType)
 enumerateTypes (LiteralE    IntExpr{}   ) = pure [KInt]
 enumerateTypes (LiteralE    FloatExpr{} ) = pure [KFloat]
@@ -90,32 +95,35 @@ enumerateTypes (FuncExpr (fmap snd -> argTypes) rt _) =
     pure [KFunc (argTypes $-> rt)]
 enumerateTypes (Call callee args) = do
     calleeTypes <- Set.toList <$> enumerateTypes callee
-    -- Possible types for each arg
-    argTypes    <- fmap Set.toList <$> mapM enumerateTypes args
-    -- Possible sequences of arg types
-    let argSequences = cartprod argTypes
-    let returnTypes =
-            catMaybes $ zipWith returnsWithArgs argSequences calleeTypes
+    argTypeSets <- fmap Set.toList <$> mapM enumerateTypes args
 
+    -- Possible sequences of arg types
+    let argTypeSequences = cartesianProduct argTypeSets
+    -- Possible combinations of call types and arg types
+    let callCombinations =
+            [ (c, as) | c <- calleeTypes, as <- argTypeSequences ]
+
+    let returnTypes = catMaybes $ uncurry returnsWithArgs <$> callCombinations
     return (Set.fromList returnTypes)
   where
-    returnsWithArgs :: [KType] -> KType -> Maybe KType
-    returnsWithArgs argTypes (KFunc (Signature argTypes' rt))
+    returnsWithArgs :: KType -> [KType] -> Maybe KType
+    returnsWithArgs (KFunc (Signature argTypes' rt)) argTypes
         | argTypes == argTypes' = Just rt
     returnsWithArgs _ _ = Nothing
 
+-- | Check that an expression has a certain type. If it applies, return the
+-- expression with the type applied. If not, throw an appropriate error.
 check :: MonadTC m => KType -> AST 'Expr TypeAnnotated -> m (AST 'Expr Typed)
 check expectedType (IdentifierE ident) = lookupName ident >>= \case
     (Binding _ availableTypes)
         | expectedType `Set.member` availableTypes -> pure
-        $  IdentifierE (typeAnnotate expectedType ident)
+            (IdentifierE (typeAnnotate expectedType ident))
         | otherwise -> throwError
             (UnavailableType (Set.toList availableTypes) expectedType)
 check expectedType (Call callee args) = do
     (typedArgs, argTypes) <- unzip <$> mapM infer args
     typedCallee           <- check (KFunc (argTypes $-> expectedType)) callee
     return (Call typedCallee typedArgs)
-
 check expectedType expr = do
     (typedExpr, inferedType) <- infer expr
     assert (expectedType == inferedType)
@@ -126,13 +134,15 @@ check expectedType expr = do
 ---------- Statements ----------
 --------------------------------
 
+-- | Try to infer the return type of a statement. If it can be typed, return the
+-- typed statement and the inferred return type. If not, throw an appropriate
+-- error
 inferReturns
     :: MonadTC m => AST 'Stmt TypeAnnotated -> m (AST 'Stmt Typed, KType)
 inferReturns (ExprStmt expr ) = first ExprStmt <$> infer expr
 inferReturns (Block    stmts) = withState id $ do
     (typedStatements, statementReturnTypes) <- unzip <$> mapM inferReturns stmts
     return (Block typedStatements, lastDef KUnit statementReturnTypes)
-   -- return $ lastDef (Set.singleton KUnit) statementTypes
 inferReturns (While (WhileStmt cond blk)) = do
     typedCond     <- check KBool cond
     (typedBlk, _) <- inferReturns blk
@@ -176,12 +186,14 @@ inferReturns (Let name declaredType expr) = do
     let typedLet = Let name declaredType typedExpr
     return (typedLet, KUnit)
 
+-- | Try to infer the binding an accessor refers to.
 inferAccessor
     :: MonadTC m
     => WriteAccess (AnnotatedName 'NoAnnotation)
     -> m (WriteAccess (AnnotatedName ( 'Annotation KType)), Binding)
-inferAccessor (WriteAccess name path) =
-    lookupName (toIdentifier name) >>= \case
+inferAccessor (WriteAccess name path) = do
+    nameBinding <- lookupName (toIdentifier name)
+    case nameBinding of
         Binding mutability [baseType] -> do
             (finalType, typedPath) <- foldPath baseType path
             return
@@ -196,15 +208,17 @@ inferAccessor (WriteAccess name path) =
         => KType
         -> [AnnotatedName 'NoAnnotation]
         -> m (KType, [AnnotatedName ( 'Annotation KType)])
-    foldPath t@(KUserType _ typeFields) (Name thisField : restPath) =
-        case lookup thisField typeFields of
+    foldPath baseType@(KUserType _ baseTypeFields) (Name thisField : restPath)
+        = case lookup thisField baseTypeFields of
             Just fieldType -> do
                 (finalType, restTypedPath) <- foldPath fieldType restPath
                 return (finalType, TName thisField fieldType : restTypedPath)
-            Nothing -> throwError (NoSuchField t thisField)
+            Nothing -> throwError (NoSuchField baseType thisField)
     foldPath t []                   = pure (t, [])
     foldPath t (Name thisField : _) = throwError (NoSuchField t thisField)
 
+-- | Check that a statement returns a given type. If it does, return the typed
+-- statement, otherwise, throw an appropriate error
 checkReturns
     :: MonadTC m => KType -> AST 'Stmt TypeAnnotated -> m (AST 'Stmt Typed)
 checkReturns KUnit (ExprStmt expr) =
@@ -213,6 +227,7 @@ checkReturns KUnit (ExprStmt expr) =
 checkReturns t (ExprStmt expr) = ExprStmt <$> check t expr
 checkReturns t stmt            = do
     (typedBlk, returnType) <- inferReturns stmt
+    -- When we're expecting Unit, anything else will do
     when (t /= KUnit) $ assert (t == returnType) (UnexpectedType t returnType)
     return typedBlk
 
@@ -220,10 +235,12 @@ checkReturns t stmt            = do
 --------- Top-level AST -----
 -----------------------------
 
+-- | Try to typecheck a module
 checkProgram
     :: MonadTC m => AST 'Module TypeAnnotated -> m (AST 'Module Typed)
 checkProgram (Program decls) = Program <$> mapM checkTopLevel decls
 
+-- | Try to typecheck a top-level declaration
 checkTopLevel
     :: MonadTC m => AST 'TopLevel TypeAnnotated -> m (AST 'TopLevel Typed)
 checkTopLevel (FuncDef name args rt body) =
@@ -234,6 +251,8 @@ checkTopLevel (DataDef name typeFields) = pure (DataDef name typeFields)
 ---------- Helpers ----------
 -----------------------------
 
+-- | Look an identifier up in the current context. If it's not present, throw an
+-- appropriate error
 lookupName :: MonadTC m => Identifier 'NoAnnotation -> m Binding
 lookupName name = gets (Map.lookup name . bindings) >>= \case
     Just binding -> pure binding
@@ -245,11 +264,12 @@ assert False err = throwError err
 
 -- | Add a set of function arguments to a TypeCtx.
 addArgs :: [(Name, KType)] -> TypeCtx -> TypeCtx
-addArgs args ctx =
-    let bindings = (Binding Constant . Set.singleton) . snd <$> args
-    in  let names = Identifier . fst <$> args
-        in  ctx <> TypeCtx Map.empty (Map.fromList (zip names bindings))
+addArgs args ctx = ctx <> TypeCtx Map.empty (Map.fromList (zip names bindings))
+  where
+    bindings = (Binding Constant . Set.singleton) . snd <$> args
+    names    = Identifier . fst <$> args
 
-cartprod :: [[a]] -> [[a]]
-cartprod []         = [[]]
-cartprod (xs : xss) = [ x : ys | x <- xs, ys <- yss ] where yss = cartprod xss
+cartesianProduct :: [[a]] -> [[a]]
+cartesianProduct []         = [[]]
+cartesianProduct (xs : xss) = [ x : ys | x <- xs, ys <- yss ]
+    where yss = cartesianProduct xss
