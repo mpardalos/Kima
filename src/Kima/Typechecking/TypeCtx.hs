1db8c53356642afa24fb7833498fe07b9ed45a13
Michalis Pardalos <m.pardalos@gmail.com>
2019-08-13 15:14:06 +0100

    Remove unnecessary functions from TypeCtx.hs

--- src/Kima/Typechecking/TypeCtx.hs
+++ src/Kima/Typechecking/TypeCtx.hs
@@ -1,7 +1,14 @@
-module Kima.Typechecking.TypeCtx where
+module Kima.Typechecking.TypeCtx
+    ( Binding(..)
+    , TypeCtx(..)
+    , Mutability(..)
+    , addType
+    , addBinding
+    )
+where
 
 import           Data.Map                       ( Map )
-import qualified Data.Map as Map
+import qualified Data.Map                      as Map
 import           Data.Set                       ( Set )
 import           GHC.Generics
 
@@ -21,9 +28,6 @@ data Binding = Binding {
 data Mutability = Constant | Variable
     deriving (Eq, Ord, Show, Generic)
 
-mapTypes :: (Set KType -> Set KType) -> Binding -> Binding
-mapTypes f b@Binding { types } = b { types = f types}
-
 addType :: TypeName -> KType -> TypeCtx -> TypeCtx
 addType name t ctx@TypeCtx { typeBindings } =
     ctx { typeBindings = Map.insert name t typeBindings }
@@ -32,14 +36,10 @@ addBinding :: Identifier 'NoAnnotation -> Binding -> TypeCtx -> TypeCtx
 addBinding n b ctx@TypeCtx { bindings } =
     ctx { bindings = Map.insertWith (<>) n b bindings }
 
-addBindings :: Map (Identifier 'NoAnnotation) Binding -> TypeCtx -> TypeCtx
-addBindings newBindings ctx@TypeCtx { bindings } =
-    ctx { bindings = bindings <> newBindings }
-
 instance Semigroup Mutability where
-    Constant <> _ = Constant
-    _ <> Constant = Constant
-    _ <> _        = Variable
+    Constant <> _        = Constant
+    _        <> Constant = Constant
+    _        <> _        = Variable
 
 instance Semigroup Binding where
     (Binding mutL typesL) <> (Binding mutR typesR) =
@@ -47,4 +47,5 @@ instance Semigroup Binding where
 
 instance Semigroup TypeCtx where
     (TypeCtx tBindsLeft bindsLeft) <> (TypeCtx tBindsRight bindsRight) =
-        TypeCtx (tBindsLeft <> tBindsRight) (Map.unionWith (<>) bindsLeft bindsRight)
+        TypeCtx (tBindsLeft <> tBindsRight)
+                (Map.unionWith (<>) bindsLeft bindsRight)
