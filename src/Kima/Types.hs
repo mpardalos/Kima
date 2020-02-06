36d46cc38f476c495b8aea0ebc8340c102b39f87
Michalis Pardalos <m.pardalos@gmail.com>
2020-02-06 13:20:50 +0000

    Move typechecker to new AST

--- src/Kima/Types.hs
+++ src/Kima/Types.hs
@@ -29,29 +29,17 @@ import           Control.Monad.State
 import           Kima.AST
 
 typecheck
-    :: TypeCtx -> AST p Desugared -> Either TypecheckingError (AST p Typed)
+    :: TypeCtx -> AST Desugared -> Either TypecheckingError (AST Typed)
 typecheck typeCtx dAST = fst <$> typecheckWithTypeCtx typeCtx dAST
 
 typecheckWithTypeCtx
-    :: TypeCtx
-    -> AST p Desugared
-    -> Either TypecheckingError (AST p Typed, TypeCtx)
+    :: TypeCtx -> AST Desugared -> Either TypecheckingError (AST Typed, TypeCtx)
 typecheckWithTypeCtx baseTypeCtx dAST = flip runStateT baseTypeCtx $ do
     typeAnnotatedAST <- resolveTypes dAST
     checkAnyAST typeAnnotatedAST
   where
-    checkAnyAST :: MonadTC m => AST p TypeAnnotated -> m (AST p Typed)
-    checkAnyAST ast@Program{} = checkProgram ast
-    checkAnyAST ast@FuncDef{} = checkTopLevel ast
-    checkAnyAST ast@DataDef{} = checkTopLevel ast
-    checkAnyAST ast@LiteralE{} = fst <$> infer ast
-    checkAnyAST ast@IdentifierE{} = fst <$> infer ast
-    checkAnyAST ast@FuncExpr{} = fst <$> infer ast
-    checkAnyAST ast@Call{} = fst <$> infer ast
-    checkAnyAST ast@ExprStmt{} = fst <$> inferReturns ast
-    checkAnyAST ast@Block{} = fst <$> inferReturns ast
-    checkAnyAST ast@While{} = fst <$> inferReturns ast
-    checkAnyAST ast@If{} = fst <$> inferReturns ast
-    checkAnyAST ast@Assign{} = fst <$> inferReturns ast
-    checkAnyAST ast@Var{} =  fst <$> inferReturns ast
-    checkAnyAST ast@Let{} = fst <$> inferReturns ast
+    checkAnyAST :: MonadTC m => AST TypeAnnotated -> m (AST Typed)
+    checkAnyAST (ModuleAST   ast) = ModuleAST <$> checkProgram ast
+    checkAnyAST (TopLevelAST ast) = TopLevelAST <$> checkTopLevel ast
+    checkAnyAST (ExprAST     ast) = ExprAST . fst <$> infer ast
+    checkAnyAST (StmtAST     ast) = StmtAST . fst <$> inferReturns ast
